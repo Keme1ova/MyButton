a16ce6ef8d6283084fdbd41f30190c140d89a399
altynaykemelova <kemelovaaltynay24@gmail.com>
2024-02-26 09:34:55 +0600

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "My_Button_Calm_Kemelova"
+include(":app")
